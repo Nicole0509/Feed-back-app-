730fa94cd20bfe89937d018b8f82202c5881eb07
Nicole0509 <niashavril0509@gmail.com>
2025-08-28 23:46:40 +0200

    dynamic variables and lists in jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App(){
-    return (<h1>Hello from the App root component</h1>);
+    const title = "Blog Posts";
+    const paragraph = "This is my blog post";
+    const comments = [
+        {id: 1, text: "Great post!"},
+        {id: 2, text: "Very informative."},
+        {id: 3, text: "Thanks for sharing!"}
+    ];
+
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{paragraph}</p>
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment) => (
+                    <li key={comment.id}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
     import React from 'react';
     import ReactDOM from 'react-dom/client';
+    import './index.css';
     import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
+    <React.StrictMode>
+            <App/>
+    </React.StrictMode>
 );
-
-ReactDOM.createRoot(document.querySelector('.random')).render(
-    <h1>This is from the class random</h1> )
