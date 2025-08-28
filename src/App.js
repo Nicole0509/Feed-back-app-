949289edd12319e9639bd0578786e9b9a1df906a
Nicole0509 <niashavril0509@gmail.com>
2025-08-29 00:01:51 +0200

    conditionals in jsx

--- src/App.js
+++ src/App.js
@@ -4,19 +4,43 @@ function App(){
     const comments = [
         {id: 1, text: "Great post!"},
         {id: 2, text: "Very informative."},
-        {id: 3, text: "Thanks for sharing!"}
+        {id: 3, text: "Thanks for sharing!"},
+        {id: 4, text: "See you next time!"}
     ];
 
-    return (
-        <div className="container">
-            <h1>{title}</h1>
-            <p>{paragraph}</p>
+    const loading = false;
+    const showComments = true;
+
+    const commentBlock = (
+        <div className="comments">
             <h3>Comments ({comments.length})</h3>
             <ul>
                 {comments.map((comment) => (
                     <li key={comment.id}>{comment.text}</li>
                 ))}
             </ul>
+        </div>
+    )
+
+    if(loading) return <h1>Loading...</h1>;
+
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{paragraph}</p>
+            {showComments && commentBlock} {/* same as below but using && operator instead of ternary operator and also having a variable store the whole comment block instead*/}
+
+            {/* {showComments ? (
+                <div className="comments">
+                    <h3>Comments ({comments.length})</h3>
+                    <ul>
+                        {comments.map((comment) => (
+                            <li key={comment.id}>{comment.text}</li>
+                        ))}
+                    </ul>
+                </div>
+            ) : null} */}
+            
         </div>
     );
 }
