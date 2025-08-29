3d2f2e2b173f1960d5b24a849adab6742545ee4b
Nicole0509 <niashavril0509@gmail.com>
2025-08-29 08:49:52 +0200

    adding style using props injsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types'
 
-function Header({text = 'FeedBack UI'}) {
+function Header({text = 'FeedBack UI', bgColor = 'rgba(0,0,0,0.4)', textColor = '#ff6a95' }) { 
+    // textColor = '#c3ff6aff' 
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
   return (
-    <header>
+    <header style={headerStyles}>
         <div className="container">
             <h2>{text}</h2> 
       
@@ -11,14 +16,11 @@ function Header({text = 'FeedBack UI'}) {
 )
 }
 
-// Though they both seem to not work
 
-Header.defaultProps = {
-    text: 'FeedBack UI',
-}
-
-Header.prototypes = {
-    text: PropTypes.string,
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 }
 
 export default Header
